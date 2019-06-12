13256568c201ed25997a32973df76339da411360
Binha <bianca.caldas@gmail.com>
2019-06-12 20:20:53 +0000

    Add more conditional examples

--- conditionals.js
+++ conditionals.js
@@ -35,3 +35,11 @@ if (temperature < 0) {
 // some comment
 
 console.log("Now you're ready to go outside!");
+
+
+const isCitizen = false;
+const age = 26;
+
+if (isCitizen && age > 18) {
+  console.log("You are eligible to vote.");
+}
