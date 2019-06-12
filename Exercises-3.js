a8a0610d19ad154c9e02ccba06ece2ca80a246db
Binha <bianca.caldas@gmail.com>
2019-06-12 20:08:45 +0000

    inicial file

---
+++ Exercises-3.js
@@ -0,0 +1,3 @@
+console.log("Hello, World");
+false;
+7 + 6;
---
+++ tinkering.js
@@ -0,0 +1,13 @@
+function multiply (a,b) {
+    return a*b;
+} 
+const result = multiply(2,4);
+console.log(result);
+
+console.log(10*4+2)
+console.log(120/3+5-3)
+console.log("Hello"+"world");
+console.log("Hello + 5*10")
+console.log("Hello" + 5*10)
+var quote = "Stay awhile and listen!";
+console.log(quote[6]);
